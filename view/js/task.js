84ec72b3187e3d0cdc180fb63b440a1426874f5c
Everton de Oliveira <1112everton@gmail.com>
2022-09-07 15:07:18 -0400

    evento-js

---
+++ view/js/task.js
@@ -0,0 +1,22 @@
+
+
+
+
+var addTarefa = document.querySelector(".adicionar-tarefa");
+
+console.log(addTarefa);
+
+addTarefa.addEventListener("click", function(){
+    event.preventDefault();
+
+    var maeDeTodasAsDivs = document.querySelector(".list-group");
+    console.log("oi",maeDeTodasAsDivs);
+    var form = document.querySelector("#add-Tarefa");
+    var titulo = form.titulo.value;
+    var dataLimite = form.dataLimite.value;
+    var descricao = form.descricao.value;
+    var categoria = form.opcao.value;
+    //console.log(titulo, dataLimite, descricao, categoria);
+
+    var nova = document.createElement("div");
+});
