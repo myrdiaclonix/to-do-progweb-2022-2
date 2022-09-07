9ab76ef29428d76be803b6419b4e91c7c0c793bb
Eduardo Santos Luz <edyven100@gmail.com>
2022-09-06 23:04:46 -0400

    Login page updated

--- view/js/login.js
+++ view/js/login.js
@@ -1,6 +1,42 @@
-const e = document.getElementsByTagName("a")[0];
-e.addEventListener("click", changeView);
+// Função: alterar as paginas do login (entre Login e Cadastro)
+$('.btn-alter-sing-page').on("click", function(e) {
 
+    let el = $(this).attr("href");
+    let page = $(el);
+    let allPages = $(".sing-page");
+
+    allPages.addClass("fade");
+
+    setTimeout(function() {
+        allPages.removeClass("show fade");
+        page.addClass("show");
+    }, 200);
+
+});
+
+// Função: validar o formulário de login
+$('#form-login').on("submit", function(e) {
+    
+    e.preventDefault();
+
+    /* Inserir todo o código de tratamento */
+    
+    alert("Fiz Login!");
+
+});
+
+// Função: validar o formulário de cadastro
+$('#form-cadastro').on("submit", function(e) {
+
+    e.preventDefault();
+
+    /* Inserir todo o código de tratamento */
+
+    alert("Fiz Cadastro!");
+
+});
+
+/* version by Jorge **OLD**
 function changeView() {
     let a = document.getElementById("signIn");
     let b = document.getElementById("signUp");
@@ -13,3 +49,7 @@ function changeView() {
         }, 50);
     }, 100);
 }
+
+const e = document.getElementsByTagName("a")[0];
+e.addEventListener("click", changeView);
+*/
